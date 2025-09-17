77ef246088fab0b95c46154c8af96dea23e84685
Noxde <mateo.tesorieri@hotmail.com>
2025-09-17 16:00:06 -0300

    Add onChange, placeholder, label and name props

--- src/components/ui/inputLabel.jsx
+++ src/components/ui/inputLabel.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Label } from "./label";
 import { Input } from "./input";
 
-function InputLabel({ readOnly, value }) {
+function InputLabel({ label, placeholder, name, readOnly, value, onChange }) {
   return (
     <div className="flex-1 flex flex-col gap-1">
-      <Label className="text-md">Motivo de Consulta</Label>
-      <Input value={value} readOnly={readOnly} />
+      <Label className="text-md">{label}</Label>
+      <Input
+        name={name}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+        readOnly={readOnly}
+      />
     </div>
   );
 }
