1b15f4f362266ae85858820497d822ffe8d8311a
Nahuel <nahuelnicolasluna@hotmail.com>
2025-09-17 00:56:26 -0300

    Se agrega schema.sql con tabla Patients y se ignora bd test Se crea la tabla Patients y se agrega base de datos test en .gitignore

---
+++ src-tauri/schema.sql
@@ -0,0 +1,14 @@
+BEGIN;
+        CREATE TABLE IF NOT EXISTS patients (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            surname TEXT NOT NULL,
+            national_id varchar(20) UNIQUE NOT NULL,
+            phone varchar(25) NOT NULL,
+            medicare TEXT,
+            medicare_number varchar(50),
+            sex TEXT CHECK(sex IN ('M', 'F', '-')) NOT NULL,
+            genre TEXT NOT NULL
+        );
+
+COMMIT;
