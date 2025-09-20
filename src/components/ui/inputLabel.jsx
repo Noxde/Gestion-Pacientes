4420e4daf3c720a0572dd6a19ce7969da7b19355
Noxde <mateo.tesorieri@hotmail.com>
2025-09-20 15:23:59 -0300

    Add classname prop

--- src/components/ui/inputLabel.jsx
+++ src/components/ui/inputLabel.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Label } from "./label";
 import { Input } from "./input";
 
-function InputLabel({ label, placeholder, name, readOnly, value, onChange }) {
+function InputLabel({
+  className,
+  label,
+  placeholder,
+  name,
+  readOnly,
+  value,
+  onChange,
+}) {
   return (
     <div className="flex-1 flex flex-col gap-1">
       <Label className="text-md">{label}</Label>
       <Input
+        className={className}
         name={name}
         onChange={onChange}
         placeholder={placeholder}
