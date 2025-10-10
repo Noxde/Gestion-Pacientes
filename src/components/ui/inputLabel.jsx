af80f3152bad1c43553dab93ed26cc931af8a5f6
Noxde <mateo.tesorieri@hotmail.com>
2025-10-09 21:29:53 -0300

    Use text-secondary

--- src/components/ui/inputLabel.jsx
+++ src/components/ui/inputLabel.jsx
@@ -12,7 +12,7 @@ function InputLabel({
   onChange,
 }) {
   return (
-    <div className="flex-1 flex flex-col gap-1">
+    <div className="flex-1 flex flex-col gap-1 text-text-secondary">
       <Label className="text-md">{label}</Label>
       <Input
         className={className}
