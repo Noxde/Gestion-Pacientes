e7eaec06fe4c409c659e7994f3c86f8cff15a8fc
Noxde <mateo.tesorieri@hotmail.com>
2025-11-12 18:19:46 -0300

    Add dialog plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -29,6 +29,7 @@ struct AppData {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .manage(AppData {
             conn: Mutex::new(None),
             data_dir: Mutex::new(None),
