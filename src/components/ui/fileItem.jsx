2aec4d140c92a50b3e45c956ebd466d1dbfcacdf
Noxde <mateo.tesorieri@hotmail.com>
2025-11-13 16:11:16 -0300

    Show list of uploaded files

---
+++ src/components/ui/fileItem.jsx
@@ -0,0 +1,37 @@
+import { convertFileSrc } from "@tauri-apps/api/core";
+import { Button } from "./button";
+import { File, X } from "lucide-react";
+
+function FileItem({ file, onClick }) {
+  const fileExtension = file.name.split(".")[1];
+  const isImage =
+    fileExtension === "jpeg" ||
+    fileExtension === "png" ||
+    fileExtension === "jpg";
+
+  return (
+    <div className="bg-[#f9fafb] p-2 rounded-md flex items-center justify-between">
+      <div>
+        {isImage ? (
+          <img
+            className="inline-block object-fill size-15 mr-2 rounded-md"
+            src={convertFileSrc(file.path)}
+            alt=""
+          />
+        ) : (
+          <File className="inline-block mx-5 text-text-secondary" />
+        )}
+        <span className="text-text-secondary">{file.name}</span>
+      </div>
+      <Button
+        onClick={() => onClick(file)}
+        variant="ghost"
+        className="cursor-pointer"
+      >
+        <X />
+      </Button>
+    </div>
+  );
+}
+
+export default FileItem;
--- src/components/ui/imageUpload.jsx
+++ src/components/ui/imageUpload.jsx
@@ -2,43 +2,62 @@ import { Upload } from "lucide-react";
 import { Label } from "./label";
 import { open } from "@tauri-apps/plugin-dialog";
 import { basename } from "@tauri-apps/api/path";
+import FileItem from "./fileItem";
 
-function ImageUpload({ setVisit, setPaths }) {
+function ImageUpload({ setFiles, files }) {
+  function handleRemove(e) {
+    setFiles((prev) => prev.filter((x) => x.name !== e.name));
+  }
+
   return (
-    <div className="">
+    <div>
       <Label className="text-md text-text-secondary mb-1">
         Adjuntar imagenes
       </Label>
 
       <label
         onClick={async (e) => {
-          const files = await open({
+          const selectedFiles = await open({
             multiple: true,
             directory: false,
           });
-          if (!files) return;
+          if (!selectedFiles) return;
+          setIsLoading(true);
 
           const docs = await Promise.all(
-            files.map(async (x) => ({
+            selectedFiles.map(async (x) => ({
               name: await basename(x),
               path: x,
             }))
           );
 
-          setVisit((prev) => ({
-            ...prev,
-            docs,
-          }));
+          setFiles((prev) => prev.concat(docs));
         }}
         className=""
         htmlFor="upload"
       >
-        <div className="flex flex-col items-center bg-[#f9fafb] rounded-md p-20 hover:opacity-70 cursor-pointer">
+        <div
+          className={`flex flex-col items-center justify-center bg-[#f9fafb] rounded-md hover:opacity-70 cursor-pointer ${
+            !files.length ? "p-20" : "p-5"
+          }`}
+        >
           <Upload className="text-text-secondary size-10" />
           <p className="text-text-secondary font-bold">Click para subir</p>
-          <p className="text-text-secondary text-sm">PNG o JPG</p>
+          {!files.length && (
+            <p className="text-text-secondary text-sm">
+              Archivos permitidos (.jpg/jpeg, .png, .mp4, .docx, .pdf)
+            </p>
+          )}
         </div>
       </label>
+      {/* Files list */}
+      {files.length ? (
+        <div className="flex flex-col gap-2 mt-2 max-h-[20dvh] overflow-y-auto">
+          {files.map((x) => (
+            <FileItem file={x} onClick={handleRemove} />
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 }
