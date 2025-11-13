90ca8bf2c2833fb0b286905392231e0f272d8fbf
Noxde <mateo.tesorieri@hotmail.com>
2025-11-13 20:49:56 -0300

    Use Dialog to show the image on click

--- src/components/ui/image.jsx
+++ src/components/ui/image.jsx
@@ -1,12 +1,32 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 function Image({ src, name }) {
   return (
-    <div className="rounded-2xl overflow-hidden relative">
-      {/* Name */}
-      <div className="absolute z-50 bg-linear-to-t from-[#000000e8] to-transparent p-2 h-full w-full flex items-end">
-        <span className="text-white font-semibold">{name}</span>
-      </div>
-      <img className="w-full h-full inline-block object-fill" src={src} />
-    </div>
+    <Dialog>
+      <DialogContent className="max-h-[calc(100svh-20px)] w-fit flex flex-col items-center overflow-hidden">
+        <DialogTitle className="self-start">Image</DialogTitle>
+        <img className="inline-block max-h-[calc(100svh-150px)]" src={src} />
+        <DialogHeader className="self-start font-medium">{name}</DialogHeader>
+      </DialogContent>
+      <DialogTrigger asChild className="cursor-pointer">
+        <div className="rounded-2xl overflow-hidden relative">
+          {/* Name */}
+          <div className="absolute z-50 bg-linear-to-t from-[#000000e8] to-transparent p-2 h-full w-full flex items-end">
+            <span className="text-white font-semibold">{name}</span>
+          </div>
+          <img
+            className="w-full h-full inline-block object-cover object-center"
+            src={src}
+          />
+        </div>
+      </DialogTrigger>
+    </Dialog>
   );
 }
 
