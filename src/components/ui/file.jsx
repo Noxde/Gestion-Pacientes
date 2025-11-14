b967d0c88dbf53719b4cda80557fd03cad54e169
Noxde <mateo.tesorieri@hotmail.com>
2025-11-14 16:46:58 -0300

    Click a document to open it on the default app

--- src/components/ui/file.jsx
+++ src/components/ui/file.jsx
@@ -1,8 +1,14 @@
+import { openPath } from "@tauri-apps/plugin-opener";
 import { File as FileIcon } from "lucide-react";
 
 function File({ file }) {
   return (
-    <div className="h-full rounded-2xl p-2 border bg-white flex flex-col justify-center items-center gap-2">
+    <div
+      onClick={async () => {
+        await openPath(file.path);
+      }}
+      className="h-full rounded-2xl p-2 border bg-white flex flex-col justify-center items-center gap-2 cursor-pointer"
+    >
       <FileIcon className="text-text-secondary" />
       <span
         className="text-text-secondary truncate w-full text-center"
